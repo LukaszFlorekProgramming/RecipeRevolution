69c394dbfa7ef582bf484f0b172e24c21a309cc8
Lukasz Florek <lukaszflorek2@gmail.com>
2023-11-30 12:54:20 +0100

    refactor pages ,MyRecipes Recipes and Recipe

--- RecipeRevolutionBlazor/Models/RecipeDto.cs
+++ RecipeRevolutionBlazor/Models/RecipeDto.cs
@@ -2,6 +2,7 @@
 {
     public class RecipeDto
     {
+        public int RecipeId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Instructions { get; set; }
