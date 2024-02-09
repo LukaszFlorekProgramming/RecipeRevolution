1949a167ea76536939a3c84f643d4b66fec1a8d4
Lukasz Florek <lukaszflorek2@gmail.com>
2024-02-09 11:13:53 +0100

    change Entity Difficulty Level

--- RecipeRevolution.Domain/Models/UpdateRecipeDto.cs
+++ RecipeRevolution.Domain/Models/UpdateRecipeDto.cs
@@ -6,7 +6,7 @@
         public string Description { get; set; }
         public string Instructions { get; set; }
         public int PreparationTime { get; set; }
-        public string DifficultyLevel { get; set; }
+        public short DifficultyLevel { get; set; }
         public short Portions { get; set; }
         public string? MainImage { get; set; }
 
---
+++ RecipeRevolution.Persistance/Migrations/20240209101224_changeEntityDifficultyLevel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeRevolution.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeEntityDifficultyLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "DifficultyLevel",
+                table: "Recipes",
+                type: "smallint",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DifficultyLevel",
+                table: "Recipes",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint",
+                oldMaxLength: 10);
+        }
+    }
+}
