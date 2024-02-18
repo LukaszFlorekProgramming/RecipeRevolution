364d8c6ffa48451923ecab89338e83dc6167fa39
Lukasz Florek <lukaszflorek2@gmail.com>
2024-02-18 14:21:32 +0100

    refactor register page

--- RecipeRevolutionBlazorApp/Models/User/UpdateUserDto.cs
+++ RecipeRevolutionBlazorApp/Models/User/UpdateUserDto.cs
@@ -11,7 +11,8 @@ namespace RecipeRevolutionBlazorApp.Models.User
         [StringLength(50, ErrorMessage = "Nazwisko może zawierać maksymalnie 50 znaków.")]
         public string LastName { get; set; }
         [RegularExpression("Male|Female|Other", ErrorMessage = "Nieprawidłowa płeć.")]
-        public string? Gender { get; set; }
+        [Required(ErrorMessage = "Pole płeć jest wymagane.")]
+        public string Gender { get; set; }
         [Range(typeof(DateTime), "1/1/1920", "1/1/2024", ErrorMessage = "Data urodzenia musi być między 01/01/1920 a 01/01/2024.")]
         public DateTime? DateOfBirth { get; set; }
         [StringLength(50, ErrorMessage = "Narodowość może zawierać maksymalnie 50 znaków.")]
